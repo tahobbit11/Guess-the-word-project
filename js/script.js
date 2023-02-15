bfa2fb5d6005f53d99504ca67061e8c81d99f00f
Taylor H <tttahobbs@gmail.com>
2023-02-15 16:26:06 -0600

    Added a validating input function

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const guessedLetters = document.querySelector(".guessed-letters");
+const guessedLettersElement = document.querySelector(".guessed-letters");
 const guessedLetterButton = document.querySelector(".guess");
 const letterInput = document.querySelector(".letter")
 const wordInProgress = document.querySelector(".word-in-progress");
@@ -8,6 +8,7 @@ const message = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
 
 const word = "Mangolia";
+const guessedLetters = [];
 
 const holder = function(word){
     const holderLetters = [];
@@ -20,4 +21,41 @@ const holder = function(word){
 
 holder(word);
 
+guessedLetterButton.addEventListener("click", function(e){
+    e.preventDefault(); //Can press the button to use but also allows enter key
+    message.innerText = ""; //Empty message
+    const guess = letterInput.value;
+    const goodGuess = validateInput(guess);
 
+    if (goodGuess) {
+        makeGuess(guess);
+    }
+
+    letterInput.value = "";
+});
+
+const validateInput = function(input){
+    const acceptedLetter = /[a-zA-Z]/;
+    if (input.length === 0){
+        //Checks if anything was entered
+        message.innerText = "Please enter a letter.";
+    } else if (input.length > 1){
+        //Checks if too many things are entered
+        message.innerText = "Please enter only one letter.";
+    } else if (!input.match(acceptedLetter)){
+        //Checks if it a letter and not a number
+        message.innerText = "Please enter a letter from a to z.";
+    } else {
+       return input; 
+    }
+};
+
+const makeGuess = function(guess){
+    guess = guess.toUpperCase();
+    if (guessedLetters.includes(guess)){
+        message.innerText = "You've already guessed this letter. Try again";
+    } else {
+        guessedLetters.push(guess);
+        console.log(guessedLetters);
+    }
+};
